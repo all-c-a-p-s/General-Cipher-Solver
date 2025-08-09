c6f01730c133e862eb4db02618bcba2234ae03f7
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2025-08-09 21:59:40 +0200

    much better CNN
    validation loss 0.33
    test accuracy 0.88

---
+++ main.py
@@ -0,0 +1,61 @@
+import keras
+
+import features
+from tabulate import tabulate
+
+
+def fmt(s):
+    return "".join(c.upper() for c in s if c.upper().isupper() or c.isdigit())
+
+
+int_to_name = {
+    0: "ADFGX",
+    1: "Autokey",
+    2: "Bifid",
+    3: "Columnar Transposition",
+    4: "Four-Square",
+    5: "Hill",
+    6: "Monoalphabetic Substitution",
+    7: "Nihilist Substitution",
+    8: "Playfair",
+    9: "Polybius",
+    10: "Railfence",
+    11: "Two-Square",
+    12: "Vigenere",
+}
+
+
+def convert_from_int(x):
+    return int_to_name[x]
+
+
+model = keras.models.load_model("models/cnn.keras")
+
+
+def analyse_ct(filename, threshold=0.01):
+    print(
+        f"Results of analysing ciphertext (only ciphers with likelihood > {threshold} shown):"
+    )
+    with open(filename, "r") as file:
+        ct = fmt(file.read().strip())
+
+    fs = features.get_all_features(ct).reshape(1, 46)
+    probs = model.predict(fs, verbose=0)[0]
+
+    likely = []
+
+    for i, x in enumerate(probs):
+        if x > threshold:
+            likely.append((convert_from_int(i), x))
+
+    likely.sort(key=lambda x: x[1], reverse=True)
+
+    return likely
+
+
+def pretty_print(results):
+    print(tabulate(results, headers=["Cipher Type", "Probability"], tablefmt="grid"))
+
+
+results = analyse_ct("ciphertext.txt")
+pretty_print(results)
