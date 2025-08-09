3a4d0aaeadde4a6e64d126114cf1cb71ed7ffd9d
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2025-08-09 12:15:00 +0200

    single HL dnn
    32 neuron HL
    
    INFO:root:evaluating model
    INFO:root:train accuracy: 0.8288
    INFO:root:test accuracy: 0.8292
    adfgx: 1.0000, confusions: []
    autokey: 0.9956, confusions: []
    bifid: 0.9266, confusions: []
    columnar: 0.4756, confusions: [railfence(0.5244)]
    foursquare: 0.4697, confusions: [playfair(0.1360),twosquare(0.3858)]
    hill: 0.8918, confusions: [autokey(0.0531),vigenere(0.0526)]
    monosub: 0.9970, confusions: []
    nihilist: 0.9996, confusions: []
    playfair: 0.7550, confusions: [foursquare(0.1671),twosquare(0.0658)]
    polybius: 1.0000, confusions: []
    railfence: 0.6209, confusions: [columnar(0.3791)]
    twosquare: 0.7126, confusions: [foursquare(0.2024),playfair(0.0694)]
    vigenere: 0.9355, confusions: []

---
+++ datagen/py/nn.py
@@ -0,0 +1,40 @@
+import logging
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input
+from sklearn.metrics import confusion_matrix
+
+import data
+import analyse
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info("about to start loading data")
+X_train, X_test, y_train, y_test = data.load_data("../data/cipher_data.csv")
+logging.info("done loading data")
+
+inputs = Input(shape=(47,))
+dense = Dense(32, activation="relu")(inputs)
+outputs = Dense(13, activation="softmax")(dense)
+
+model = Model(inputs, outputs)
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(X_train, y_train, epochs=6, batch_size=64, validation_data=(X_test, y_test))
+
+logging.info("evaluating model")
+train_loss, train_acc = model.evaluate(X_train, y_train, verbose=0)
+test_loss, test_acc = model.evaluate(X_test, y_test, verbose=0)
+
+logging.info(f"train accuracy: {train_acc:.4f}")
+logging.info(f"test accuracy: {test_acc:.4f}")
+
+y_pred_probs = model.predict(X_test, verbose=0)
+y_pred_classes = np.argmax(y_pred_probs, axis=1)
+y_test_classes = np.argmax(y_test, axis=1) if y_test.ndim > 1 else y_test
+
+
+cf = confusion_matrix(y_test_classes, y_pred_classes)
+analyse.analyse_confusion_matrix(cf)
+
+model.save("../models/dnn.keras")
